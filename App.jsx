fe88dbcba1b78175ad9b130cd0498def81fc02b1
Enzo Takashi Martinatto Borges <112429924+enzotmborges@users.noreply.github.com>
2024-03-18 07:32:41 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+
+function WorldClock({ location, time }) {
+  return (
+    <div className="world-clock">
+        <h2> ⌚ </h2>
+        <h2> {location} </h2>
+    <p className="clock-text"> {time} </p>
+  </div>
+  );
+}
+
+function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Relógio Mundial 🌍</h1>
+      <div className="clock-container">
+        <WorldClock location="Brasília" 
+                    time={time.toLocaleTimeString('pt-BR', { 
+                    timeZone: 'America/Sao_Paulo' })} />
+
+        <WorldClock location="Nova York" 
+                    time={time.toLocaleTimeString('en-US', { 
+                    timeZone: 'America/New_York' })} />
+
+        <WorldClock location="Londres" 
+                    time={time.toLocaleTimeString('en-GB', { 
+                    timeZone: 'Europe/London' })} />
+
+        <WorldClock location="Tóquio" 
+                    time={time.toLocaleTimeString('ja-JP', { 
+                    timeZone: 'Asia/Tokyo' })} />
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
